1a885b19ec65a88527005e16b02576453973e311
langsmith <langstonlmcs@gmail.com>
2019-08-06 18:29:27 -0700

    [android] removed LocationComponentOptions from LocationAnimatorCoordinator class and test for pulsing circle

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/location/LocationComponent.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/location/LocationComponent.java
@@ -1244,8 +1244,7 @@ public final class LocationComponent {
     locationAnimatorCoordinator = new LocationAnimatorCoordinator(
       mapboxMap.getProjection(),
       MapboxAnimatorSetProvider.getInstance(),
-      MapboxAnimatorProvider.getInstance(), options
-    );
+      MapboxAnimatorProvider.getInstance());
     locationAnimatorCoordinator.setTrackingAnimationDurationMultiplier(options
       .trackingAnimationDurationMultiplier());
 
